14f3fba3b45e1d71b96b4d33eba76b41152860a7
BigFatDuck1 <clementho999@gmail.com>
2023-12-04 15:09:49 +0800

    Added basic html frame

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 
 import './styles.css';
 
-const startUp = () => {
-    const test = document.createElement('div');
-    test.textContent = 'Hello World';
-    document.querySelector(".container").appendChild(test);
-}
-
-startUp();
